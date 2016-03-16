016b60075ee5a66a60a3a2138109d866d45cfc77
Mathias Klippinge <mathias.klippinge@klarna.com>
2016-03-16 07:49:11 +0100

    2 whitespaces as tabs, not 4

--- config.js
+++ config.js
@@ -2,145 +2,145 @@
 require('dotenv').load();
 
 var config = {
-    env:  'prod',
+  env:  'prod',
 
-    host: '0.0.0.0',
-    port: process.env.PORT || 5000,
+  host: '0.0.0.0',
+  port: process.env.PORT || 5000,
 
-    // Available themes:
-    // + night-blue
-    // + light-grey
-    // + yellow
-    // + light-yellow
-    // + bordeau
-    theme: 'night-blue',
+  // Available themes:
+  // + night-blue
+  // + light-grey
+  // + yellow
+  // + light-yellow
+  // + bordeau
+  theme: 'night-blue',
 
-    // clients configs
-    api: {
-        weather: {
-          token: '5382d946f938337e8eb3624fb2c80b83'
+  // clients configs
+  api: {
+    weather: {
+      token: '5382d946f938337e8eb3624fb2c80b83'
+    },
+    aws: {
+      region: 'eu-west-1'
+    },
+    jenkins: {
+      baseUrl: 'https://my-jenkins.com',
+      auth: {
+        user:   'me',
+        password: 'me'
+      }
+    }
+  },
+
+  // define duration between each dashboard rotation (ms)
+  rotationDuration: 15000,
+
+  dashboards: [
+
+    // first dashboard
+    {
+      // 4 x 3 dashboard
+      columns: 4,
+      rows:  3,
+      widgets: [
+        {
+          type: 'github.user_badge',
+          user: 'klippx',
+          columns: 1, rows: 1,
+          x: 0, y: 0
         },
-        aws: {
-            region: 'eu-west-1'
+        {
+          type: 'github.repository_contributors_stats',
+          repository: 'klippx/exercism',
+          columns: 1, rows: 1,
+          x: 2, y: 0
         },
-        jenkins: {
-            baseUrl: 'https://my-jenkins.com',
-            auth: {
-                user:     'me',
-                password: 'me'
-            }
+        {
+          type: 'travis.repository',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 1, rows: 1,
+          x: 1, y: 0
+        },
+        {
+          type: 'travis.build_histogram',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 2, rows: 1,
+          x: 1, y: 1
+        },
+        {
+          type: 'time.clock',
+          columns: 1, rows: 1,
+          x: 3, y: 0
+        },
+        {
+          type: 'weather.weather',
+          city: 'Tokyo',
+          country: 'JP',
+          lang: 'en',
+          limit: 2,
+          columns: 1, rows: 1,
+          x: 0, y: 1
+        },
+        {
+          type: 'travis.build_history',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 1, rows: 2,
+          x: 3, y: 1
+        },
+        {
+          type: 'travis.build_histogram',
+          owner: 'plouc',
+          repository: 'go-gitlab-client',
+          columns: 2, rows: 1,
+          x: 1, y: 2
+        },
+        {
+          type: 'github.status',
+          columns: 1, rows: 1,
+          x: 0, y: 2
         }
+      ]
     },
 
-    // define duration between each dashboard rotation (ms)
-    rotationDuration: 15000,
-
-    dashboards: [
-
-        // first dashboard
+    // second dashboard
+    {
+      // 3 x 2 dashboard
+      columns: 3,
+      rows:  2,
+      widgets: [
         {
-            // 4 x 3 dashboard
-            columns: 4,
-            rows:    3,
-            widgets: [
-                {
-                    type: 'github.user_badge',
-                    user: 'klippx',
-                    columns: 1, rows: 1,
-                    x: 0, y: 0
-                },
-                {
-                    type: 'github.repository_contributors_stats',
-                    repository: 'klippx/exercism',
-                    columns: 1, rows: 1,
-                    x: 2, y: 0
-                },
-                {
-                    type: 'travis.repository',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 1,
-                    x: 1, y: 0
-                },
-                {
-                    type: 'travis.build_histogram',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 2, rows: 1,
-                    x: 1, y: 1
-                },
-                {
-                    type: 'time.clock',
-                    columns: 1, rows: 1,
-                    x: 3, y: 0
-                },
-                {
-                    type: 'weather.weather',
-                    city: 'Tokyo',
-                    country: 'JP',
-                    lang: 'en',
-                    limit: 2,
-                    columns: 1, rows: 1,
-                    x: 0, y: 1
-                },
-                {
-                    type: 'travis.build_history',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 2,
-                    x: 3, y: 1
-                },
-                {
-                    type: 'travis.build_histogram',
-                    owner: 'plouc',
-                    repository: 'go-gitlab-client',
-                    columns: 2, rows: 1,
-                    x: 1, y: 2
-                },
-                {
-                    type: 'github.status',
-                    columns: 1, rows: 1,
-                    x: 0, y: 2
-                }
-            ]
+          type: 'travis.build_history',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 1, rows: 2,
+          x: 0, y: 0
         },
-
-        // second dashboard
         {
-            // 3 x 2 dashboard
-            columns: 3,
-            rows:    2,
-            widgets: [
-                {
-                    type: 'travis.build_history',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 2,
-                    x: 0, y: 0
-                },
-                {
-                    type: 'github.user_badge',
-                    user: 'plouc',
-                    columns: 1, rows: 1,
-                    x: 2, y: 0
-                },
-                {
-                    type: 'travis.repository',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 1, rows: 1,
-                    x: 1, y: 0
-                },
-                {
-                    type: 'travis.build_histogram',
-                    owner: 'plouc',
-                    repository: 'mozaik',
-                    columns: 2, rows: 1,
-                    x: 1, y: 1
-                }
-            ]
+          type: 'github.user_badge',
+          user: 'plouc',
+          columns: 1, rows: 1,
+          x: 2, y: 0
+        },
+        {
+          type: 'travis.repository',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 1, rows: 1,
+          x: 1, y: 0
+        },
+        {
+          type: 'travis.build_histogram',
+          owner: 'plouc',
+          repository: 'mozaik',
+          columns: 2, rows: 1,
+          x: 1, y: 1
         }
-    ]
+      ]
+    }
+  ]
 };
 
 module.exports = config;
