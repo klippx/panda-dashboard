00165dd5d65749c40bdde1ece803661dd045408b
Mathias Klippinge <mathias.klippinge@klarna.com>
2016-04-08 08:33:57 +0200

    Started adding jira and aws

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ var mozaik = new (require('mozaik'))(require('./config'));
 
 mozaik.bus.registerApi('weather', require('mozaik-ext-weather/client'));
 mozaik.bus.registerApi('jenkins', require('mozaik-ext-jenkins/client'));
+mozaik.bus.registerApi('jira', require('mozaik-ext-jira/client'));
+mozaik.bus.registerApi('aws', require('mozaik-ext-aws/client'));
 
 mozaik.startServer();
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React             from 'react';
 import Mozaik            from 'mozaik/browser';
-import githubComponents  from 'mozaik-ext-github';
-import travisComponents  from 'mozaik-ext-travis';
 import timeComponents    from 'mozaik-ext-time';
 import weatherComponents from 'mozaik-ext-weather';
 import jenkinsComponents from 'mozaik-ext-jenkins';
+import jiraComponents    from 'mozaik-ext-jira';
+import awsComponents     from 'mozaik-ext-aws';
 
 
 const MozaikComponent = Mozaik.Component.Mozaik;
@@ -12,11 +12,11 @@ const ConfigActions   = Mozaik.Actions.Config;
 
 
 Mozaik.Registry.addExtensions({
-    github:  githubComponents,
-    travis:  travisComponents,
     time:    timeComponents,
     jenkins: jenkinsComponents,
-    weather: weatherComponents
+    weather: weatherComponents,
+    jira:    jiraComponents,
+    aws:     awsComponents,
 });
 
 React.render(<MozaikComponent/>, document.getElementById('mozaik'));
